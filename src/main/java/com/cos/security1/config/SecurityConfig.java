3e7c1e12227a8a4d750039d1bfc403a91d500f29
Buyoung Han <qnfudk@gmail.com>
2024-09-12 16:35:12 +0900

    Role부여 규칙 수정, 어노테이션 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌 (Bean 어노테이션)
@@ -31,9 +33,7 @@ public class SecurityConfig {
                         authorizeHttpRequests
 
                                 .requestMatchers("/user/**").authenticated()
-
                                 .requestMatchers("/admin/**").hasRole("ADMIN")
-
                                 .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")
 
                                 .anyRequest().permitAll()
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,14 +1,20 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Iterator;
 
 @Controller // View 리턴
 public class IndexController {
@@ -29,18 +35,29 @@ public class IndexController {
     }
 
     @GetMapping("/user")
-    public String user() {
-        return "user";
+    public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principal) {
+        System.out.println("Principal : " + principal);
+        // iterator 순차 출력 해보기
+        Iterator<? extends GrantedAuthority> iter = principal.getAuthorities().iterator();
+        while (iter.hasNext()) {
+            GrantedAuthority auth = iter.next();
+            System.out.println(auth.getAuthority());
+        }
+
+        return "유저 페이지입니다.";
     }
 
     @GetMapping("/admin")
-    public String admin() {
-        return "admin";
+    public @ResponseBody String admin() {
+        return "어드민 페이지입니다.";
     }
 
+    //@PostAuthorize("hasRole('ROLE_MANAGER')")
+    //@PreAuthorize("hasRole('ROLE_MANAGER')")
+    @Secured("ROLE_MANAGER")
     @GetMapping("/manager")
-    public String manager() {
-        return "manager";
+    public @ResponseBody String manager() {
+        return "매니저 페이지입니다.";
     }
 
     @GetMapping("/login")
@@ -51,7 +68,7 @@ public class IndexController {
     @PostMapping("/join")
     public String join(User user) {
         System.out.println(user);
-        user.setRole("USER");
+        user.setRole("ROLE_USER");
         String rawPassword = user.getPassword();
         String encodedPassword = bCryptPasswordEncoder.encode(rawPassword);
         user.setPassword(encodedPassword);
@@ -68,5 +85,9 @@ public class IndexController {
     public String joinForm() {
         return "joinForm";
     }
-
+    
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
 }
