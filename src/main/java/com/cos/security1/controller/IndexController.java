f4b17d01d269754fb778324d7aee433a5f61a44b
Buyoung Han <qnfudk@gmail.com>
2024-09-12 10:34:57 +0900

    Spring Security 기본설정 추가

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,13 +3,25 @@ package com.cos.security1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller // View 리턴
 public class IndexController {
 
     //localhost:8080/
-    //localhost:8080/
+    //localhost:8080
     @GetMapping({"","/"})
     public String index() {
+        // 머스태치 기본폴더 src/main/resources/
+        // View Resolver 설정 : templates (prefix), .mustache (suffix)
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String user() {
+        return "admin";
     }
 }
