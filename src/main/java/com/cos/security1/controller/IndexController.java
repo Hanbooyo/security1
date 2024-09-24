2d72396b370c512e19bc107497954074e0656631
Buyoung Han <qnfudk@gmail.com>
2024-09-24 16:27:54 +0900

    세션정보 확인, OAuth 세션정보 확인 Api작성

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,13 +27,26 @@ public class IndexController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping("/test/login")
-    public @ResponseBody String loginTest(Authentication authentication) { // DI 의존성 주입
+    public @ResponseBody String loginTest(
+            Authentication authentication,
+            @AuthenticationPrincipal PrincipalDetails userDetails) { // DI 의존성 주입
         System.out.println("/test/login ===========================");
         PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
         System.out.println("authentication = " + authentication.getPrincipal());
-        System.out.println("authentication = " + principalDetails.getUser());
+        System.out.println("PrincipalDetails = " + userDetails.getUser()); // 다운캐스팅
         return "세션 정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLogin(
+            Authentication authentication,
+            @AuthenticationPrincipal PrincipalDetails userDetails) { // DI 의존성 주입
+        System.out.println("/test/login ===========================");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication = " + oAuth2User.getAttributes());
+        return "OAuth 세션 정보 확인하기";
     }
 
     //localhost:8080/
